a469361c141f5bd286a0ccba6223038608cbdc5a
brayam <bgajar98@gmail.com>
2021-04-04 23:28:27 -0500

    Tabla autor Completamente Funcionando

---
+++ WebApi_Biblioteca.GUI/Controllers/PublicacionController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApi_Biblioteca.GUI.Models;
+
+namespace WebApi_Biblioteca.GUI.Controllers
+{
+    public class PublicacionController : Controller
+    {
+        
+        // GET: Publicacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Publicacion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Publicacion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publicacion/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publicacion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Publicacion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publicacion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Publicacion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApi_Biblioteca.GUI/Models/DTO/Publicacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_Biblioteca.GUI.Models.DTO
+{
+    public class Publicacion
+    {
+        [Display(Name = "ID")]
+        [Required]
+        public int PublicacionID { get; set; }
+
+        [Display(Name = "Tematica")]
+        [Required]
+        public string Tematica { get; set; }
+
+        [Display(Name = "Idioma")]
+        [Required]
+        public string Idioma { get; set; }
+
+        [Display(Name = "Fecha de Publicacion")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaPublicacion { get; set; }
+
+        [Display(Name = "Editorial")]
+        [Required]
+        public string Editorial { get; set; }
+
+        [Display(Name = "ID EJEMPLAR")]
+        [Required]
+        public int EjemplarID { get; set; }
+
+        [Display(Name = "Tipo Publicacion")]
+        public string TipoPublicacion { get; set; }
+    }
+
+}
